85278a72b2a1233a6f362bcecae41701ff3bd8c5
Gareth Bird <garethbird@gmail.com>
2025-04-07 12:28:17 +0100

    new tests and constructor

--- SCARS.Core/Storage/FileStorage.cs
+++ SCARS.Core/Storage/FileStorage.cs
@@ -1,4 +1,5 @@
-﻿using SCARS.Storage.Interfaces;
+﻿using System.Diagnostics.CodeAnalysis;
+using SCARS.Storage.Interfaces;
 using System.Text.Json;
 
 namespace SCARS.Storage;
@@ -9,18 +10,42 @@ public class FileStorage<T> : IDataStorage<T>, IDataPersistence<T>
     private readonly IDataStorage<T> _inner;
     private readonly string _writePath;
 
-    public FileStorage(IDataStorage<T> inner, string writeFilePath)
+    public FileStorage([NotNull] IDataStorage<T> inner, [NotNull] string writeFilePath)
     {
-        _inner = inner ?? throw new ArgumentNullException(nameof(inner));
-        _writePath = writeFilePath ?? throw new ArgumentNullException(nameof(writeFilePath));
+        _inner = inner;
+        _writePath = writeFilePath;
+
+        // Ensure that the directory exists before trying to write to the file.
+        var directory = Path.GetDirectoryName(_writePath);
+        if (!string.IsNullOrWhiteSpace(directory))
+        {
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            var directoryAttributes = File.GetAttributes(directory);
+            if (directoryAttributes.HasFlag(FileAttributes.ReadOnly))
+            {
+                throw new UnauthorizedAccessException($"Write access to the path '{directory}' is denied.");
+            }
+        }
     }
 
     public Task ClearDataAsync() => _inner.ClearDataAsync();
 
     public async Task PersistAsync()
     {
+        // Ensure the file exists; if not, create an empty file
+        if (!File.Exists(_writePath))
+        {
+            // Create an empty file (you can use File.Create, which will create the file and leave it empty)
+            using (File.Create(_writePath)) { }
+        }
+
+        // Now serialize and persist the data
         var data = await _inner.RetrieveDataAsync();
         var json = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+
+        // Write the data to the file
         File.WriteAllText(_writePath, json);
     }
 
